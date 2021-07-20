2495c4689ad0c8e53710c8715a4293bee8eed877
Saif <76686929+saif-malik-01@users.noreply.github.com>
2021-07-19 21:13:07 -0700

    code-server.sh

---
+++ code-server.sh
@@ -0,0 +1,6 @@
+#!/bash/bin
+
+
+wget https://github.com/cdr/code-server/releases/download/v3.11.0/code-server-3.11.0-linux-arm64.tar.gz &&
+tar -xvf ./code-server-3.11.0-linux-arm64.tar.gz && rm ./code-server-3.11.0-linux-arm64.tar.gz &&
+cp -r code-server-3.11.0-linux-arm64 /lib/ && ln -s /lib/code-server-3.11.0-linux-arm64/code-server /bin/
